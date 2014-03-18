22773fce94dc8fec717289bb92dd1234f526aa9b
Thomas Thune Hansen <tth@bellcom.dk>
2014-03-18 08:13:16 +0100

    #151 added filtering on duplicate lineitems on all cart views, removed console.log() from Terms.js

--- bosa_extras/js/jquery.Terms.js
+++ bosa_extras/js/jquery.Terms.js
@@ -1,6 +1,5 @@
 jQuery('document').ready(function($){
   var terms = Drupal.settings.bosa_extras.terms;
-  console.log(terms);
 
   $(".form-item-customer-profile-billing-field-conditions-und").append('<div id="termsandconditions"><div class="close">Luk</div>' + terms + '</div>');
 
